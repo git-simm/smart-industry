b8ce3e52b40a8dd5dda236c1d644e1b179fedc63
manman.si <214382122@qq.com>
2018-08-30 12:52:41 +0800

    添加Druid监控配置

---
+++ smart-industry-train-web/src/main/java/smart/industry/train/web/config/DruidConfig.java
@@ -0,0 +1,76 @@
+package smart.industry.train.web.config;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * durid-servlet配置
+ */
+@ConditionalOnProperty(value = "druid.monitor.enable",matchIfMissing = true,havingValue = "true")
+@ConfigurationProperties(prefix = "druid.monitor")
+@Configuration
+public class DruidConfig {
+    private final Logger log = LoggerFactory.getLogger(DruidConfig.class);
+    //登录用户
+    private String user;
+    //登录密码
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        log.info("init Druid Servlet Configuration ");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap();
+        initParameters.put("loginUsername", "admin");// 用户名
+        initParameters.put("loginPassword", "admin@123");// 密码
+        if(!StringUtils.isEmpty(user)){
+            initParameters.put("loginUsername", user);
+        }
+        if(!StringUtils.isEmpty(password)){
+            initParameters.put("loginPassword", password);
+        }
+        initParameters.put("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
+        initParameters.put("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
+        //initParameters.put("deny", "192.168.20.38");// IP黑名单 (存在共同时，deny优先于allow)
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
