b5e322f8884f03606814af083314e3acdc1842b4
git-simm <214382122@qq.com>
2019-01-10 22:15:32 +0800

    发布一次

--- smart-industry-train-web/src/main/java/smart/industry/train/web/MyApplicationReadyEventListener.java
+++ smart-industry-train-web/src/main/java/smart/industry/train/web/MyApplicationReadyEventListener.java
@@ -3,11 +3,13 @@ package smart.industry.train.web;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 import smart.industry.train.biz.threads.ThreadService;
 
 /**
  * 系统启动完成事件监听
  */
+@Component
 public class MyApplicationReadyEventListener implements ApplicationListener<ApplicationReadyEvent> {
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
